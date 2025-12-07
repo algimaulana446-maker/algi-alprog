f0b09c92b54faa9b26e11dc664c10a0087c89b89
algimaulana446-maker <algimaulana446@gmail.com>
2025-12-07 23:26:06 +0800

    Add files via upload
    M.Algi Maulana

---
+++ pert6.py
@@ -0,0 +1,52 @@
+#program konversi suhu
+
+celcius = float(input(("masukkan suhu anda :")))
+print("suhu anda dalam celcius :" , celcius)
+
+#fahrenheit x 9/5 x celcius
+
+fahrenheit = (9/5 * celcius) + 32
+print("suhu anda dalam fahrenheit :" , fahrenheit)
+
+#reamur
+reamur = (4/5 * celcius)
+print("suhu anda dalam reamur :" , reamur)
+
+#kelvin
+kelvin = (celcius) + 273
+print("suhu anda dalam kelvin :" , kelvin)
+
+fahrenheit = float(input(("masukkan suhu anda :")))
+print("suhu anda dalam fahrenheit : " , fahrenheit)
+
+#celcius
+celcius = (5/9 * fahrenheit) + 32
+print("suhu anda dalam celcius :" , celcius)
+#reamur
+reamur = (4/9 * fahrenheit) -32
+print("suhu anda dalam reamur :" , reamur)
+
+
+reamur = float(input(("masukkan suhu anda : ")))
+print("suhu anda dalam reamur :" , reamur)
+
+#celcius
+celcius =(5/4 * reamur)
+print("suhu anda dalam celcius :" , celcius)
+#fahrenheit
+fahrenheit = (9/4 * reamur) + 32
+print("suhu anda dalam fahrenheit :" , fahrenheit)
+#kelvin
+kelvin = (5/4 * reamur) + 273
+print("nilai anda dalam kelvin :" , kelvin)
+
+
+kelvin = float(input(("masukkan suhu anda :")))
+print("suhu anda dalam kelvin :" , kelvin)
+
+#celcius
+celcius = (kelvin) -273
+print("nilai anda dalam celcius :" , celcius)
+#reamur
+reamur = (4/5 * kelvin)-273
+print("nilai anda dalam reamur :" , reamur)
